5a2a945a4c7e410bbc20bae9c0ae6739176b1e6d
Kirill <krshka2000@gmail.com>
2025-10-22 20:54:34 +0300

    lab 6 ktp

---
+++ lab6 ktp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+try
+{
+    Console.WriteLine("Input integer number");
+    string str = Console.ReadLine();
+    int num = int.Parse(str);
+    Console.WriteLine($"Your number is {num}");
+}
+catch
+{
+    Console.WriteLine(
+        "You input not integer number"
+        );
+} 
